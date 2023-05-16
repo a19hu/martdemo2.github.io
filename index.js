baa89397068b5547ac299f923b3dd229750c431c
a19hu <132092345+a19hu@users.noreply.github.com>
2023-05-16 15:15:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-let b=0
+/*let b=0
 function cart(){
     b=b+1
     document.getElementById("count").innerText=b
 }
+document.getElementById("count").innerText=localStorage.getItem(b)*/
+function cart() {
+let key=promp
+let value= promp
+localStorage.setItem(key,value)
+document.getElementById("count").innerHTML = mkjn}
+
+cart()
+/*
+function cart() {
+  if (sessionStorage.clickcount) {
+        sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
+      } else {
+        sessionStorage.clickcount = 0;
+      }
+     document.getElementById("count").innerHTML = sessionStorage.clickcount ;
+} */
+    
+  
